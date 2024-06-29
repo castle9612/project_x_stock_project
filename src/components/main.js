c3572f7443956b321a9f532c2a5c418cab55e5cc
hyoju <nicehyoju@gmail.com>
2024-06-29 16:06:39 +0900

    메인화면 수정중

--- src/components/main.js
+++ src/components/main.js
@@ -4,11 +4,21 @@ function Main() {
 
   return (
     <div id="App">
-        <div id="header">
+        <div id="header_main">
+            <button id="logout_btn">
+                <span>로그아웃</span>
+            </button>
         </div>
-        <div id="chart">
-        </div>
-        <div id="contents">
+        <div id="body_main">
+            <div id="stock_info">
+                주가정보
+            </div>
+            <div id="chart">
+                주가그래프
+            </div>
+            <div id="contents">
+                매수,매매 등등
+            </div>
         </div>
     </div>
   );
