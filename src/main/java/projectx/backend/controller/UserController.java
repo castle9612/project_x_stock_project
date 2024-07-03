7b459cb485fb4947dad6f0e407a87535ac4d274a
seon7129 <seon7129@naver.com>
2024-07-03 21:16:59 +0900

    comment delete:

--- src/main/java/projectx/backend/controller/UserController.java
+++ src/main/java/projectx/backend/controller/UserController.java
@@ -1,13 +1,13 @@
-//package projectx.backend.controller;
-//
-//
-//import org.springframework.web.bind.annotation.RestController;
-//import projectx.backend.service.UserService;
-//
-//@RestController
-//public class UserController {
-//
-//    public UserService userService;
-//
-//
-//}
+package projectx.backend.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import projectx.backend.service.UserService;
+
+@RestController
+public class UserController {
+
+    public UserService userService;
+
+
+}
--- src/main/java/projectx/backend/entity/User.java
+++ src/main/java/projectx/backend/entity/User.java
@@ -1,11 +1,11 @@
-//package projectx.backend.entity;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//
-//@Entity
-//public class User {
-//    @Id
-//    Long id;
-//
-//}
+package projectx.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    Long id;
+
+}
