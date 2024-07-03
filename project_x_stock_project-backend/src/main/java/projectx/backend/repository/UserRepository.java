0589c321c759c99047c5220801b2e301cf67d7ef
aubit <aubit8362@naver.com>
2024-07-03 21:53:19 +0900

    commit

---
+++ project_x_stock_project-backend/src/main/java/projectx/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package projectx.backend.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import projectx.backend.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
