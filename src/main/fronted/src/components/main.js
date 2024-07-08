c90657d7fedbc59ff104db0defcdd35829c8f11f
hyoju <nicehyoju@gmail.com>
2024-07-08 18:15:58 +0900

    add fronted

--- src/main/fronted/src/components/main.js
+++ src/main/fronted/src/components/main.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Chart, ChartCanvas, CandlestickSeries, XAxis, YAxis, CrossHairCursor, MouseCoordinateX,
     MouseCoordinateY, discontinuousTimeScaleProviderBuilder } from 'react-financial-charts';
 import { timeFormat } from 'd3-time-format';
@@ -6,6 +7,31 @@ import * as d3 from 'd3-format';
 import '../css/main.css';
 
 function Main() {
+    const [chartData, setChartData] = useState([]);
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('http://34.22.73.38:8080/stock-data-for-graph', {
+                params: {
+                    stockCode: '005930',
+                    startDate: '2024-05-05',
+                    endDate: '2024-06-20'
+                }
+            });
+            const parsedData = response.data.map(item => ({
+                ...item,
+                date: new Date(item.date)
+            }));
+            setChartData(parsedData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     const GoStart = () => {
         window.location.href = '/';
     };
@@ -29,236 +55,10 @@ function Main() {
         "lastUpdated": "2024-07-07T15:30:00"
     };
 
-    const data = [
-        {
-            "date": "2024-06-20",
-            "open": 81500,
-            "high": 82200,
-            "low": 81200,
-            "close": 81600
-        },
-        {
-            "date": "2024-06-19",
-            "open": 81100,
-            "high": 82500,
-            "low": 80500,
-            "close": 81200
-        },
-        {
-            "date": "2024-06-18",
-            "open": 78700,
-            "high": 80200,
-            "low": 78600,
-            "close": 79800
-        },
-        {
-            "date": "2024-06-17",
-            "open": 79200,
-            "high": 79500,
-            "low": 78000,
-            "close": 78100
-        },
-        {
-            "date": "2024-06-14",
-            "open": 79700,
-            "high": 80500,
-            "low": 79000,
-            "close": 79600
-        },
-        {
-            "date": "2024-06-13",
-            "open": 78400,
-            "high": 79000,
-            "low": 77800,
-            "close": 78600
-        },
-        {
-            "date": "2024-06-12",
-            "open": 75300,
-            "high": 77100,
-            "low": 75200,
-            "close": 76500
-        },
-        {
-            "date": "2024-06-11",
-            "open": 75900,
-            "high": 76000,
-            "low": 75100,
-            "close": 75200
-        },
-        {
-            "date": "2024-06-10",
-            "open": 76100,
-            "high": 76600,
-            "low": 75600,
-            "close": 75700
-        },
-        {
-            "date": "2024-06-07",
-            "open": 78400,
-            "high": 78600,
-            "low": 77100,
-            "close": 77300
-        },
-        {
-            "date": "2024-06-05",
-            "open": 78000,
-            "high": 78000,
-            "low": 76800,
-            "close": 77400
-        },
-        {
-            "date": "2024-06-04",
-            "open": 74900,
-            "high": 76100,
-            "low": 74900,
-            "close": 75300
-        },
-        {
-            "date": "2024-06-03",
-            "open": 74400,
-            "high": 76400,
-            "low": 74200,
-            "close": 75700
-        },
-        {
-            "date": "2024-05-31",
-            "open": 74500,
-            "high": 74700,
-            "low": 73500,
-            "close": 73500
-        },
-        {
-            "date": "2024-05-30",
-            "open": 74800,
-            "high": 75200,
-            "low": 73500,
-            "close": 73500
-        },
-        {
-            "date": "2024-05-29",
-            "open": 77700,
-            "high": 78200,
-            "low": 75200,
-            "close": 75200
-        },
-        {
-            "date": "2024-05-28",
-            "open": 76500,
-            "high": 78000,
-            "low": 76200,
-            "close": 77600
-        },
-        {
-            "date": "2024-05-27",
-            "open": 75300,
-            "high": 78200,
-            "low": 74000,
-            "close": 77200
-        },
-        {
-            "date": "2024-05-24",
-            "open": 76800,
-            "high": 77000,
-            "low": 75700,
-            "close": 75900
-        },
-        {
-            "date": "2024-05-23",
-            "open": 77700,
-            "high": 79100,
-            "low": 77100,
-            "close": 78300
-        },
-        {
-            "date": "2024-05-22",
-            "open": 78100,
-            "high": 78700,
-            "low": 77300,
-            "close": 77700
-        },
-        {
-            "date": "2024-05-21",
-            "open": 78500,
-            "high": 79000,
-            "low": 78200,
-            "close": 78400
-        },
-        {
-            "date": "2024-05-20",
-            "open": 78100,
-            "high": 79100,
-            "low": 77900,
-            "close": 78900
-        },
-        {
-            "date": "2024-05-17",
-            "open": 78600,
-            "high": 78800,
-            "low": 77200,
-            "close": 77400
-        },
-        {
-            "date": "2024-05-16",
-            "open": 80200,
-            "high": 80300,
-            "low": 78100,
-            "close": 78200
-        },
-        {
-            "date": "2024-05-14",
-            "open": 78600,
-            "high": 78800,
-            "low": 77900,
-            "close": 78300
-        },
-        {
-            "date": "2024-05-13",
-            "open": 79400,
-            "high": 79900,
-            "low": 77600,
-            "close": 78400
-        },
-        {
-            "date": "2024-05-10",
-            "open": 80400,
-            "high": 81100,
-            "low": 78900,
-            "close": 79200
-        },
-        {
-            "date": "2024-05-09",
-            "open": 81100,
-            "high": 81500,
-            "low": 79700,
-            "close": 79700
-        },
-        {
-            "date": "2024-05-08",
-            "open": 80800,
-            "high": 81400,
-            "low": 80500,
-            "close": 81300
-        },
-        {
-            "date": "2024-05-07",
-            "open": 79600,
-            "high": 81300,
-            "low": 79400,
-            "close": 81300
-        }
-    ]
-
-    const parsedData = data.map(item => ({
-        ...item,
-        date: new Date(item.date)
-    }));
-
     const xScaleProvider = discontinuousTimeScaleProviderBuilder().inputDateAccessor(d => d.date);
-    const { data: chartData, xScale, xAccessor, displayXAccessor } = xScaleProvider(parsedData);
+    const { data: chartDataParsed, xScale, xAccessor, displayXAccessor } = xScaleProvider(chartData);
 
-    const xExtents = [xAccessor(chartData[0]), xAccessor(chartData[chartData.length - 1])];
-
+    const xExtents = [xAccessor(chartDataParsed[0]), xAccessor(chartDataParsed[chartDataParsed.length - 1])];
 
     return (
         <div id="App">
@@ -283,13 +83,13 @@ function Main() {
                         width={1100}
                         ratio={3}
                         margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
-                        data={chartData}
+                        data={chartDataParsed}
                         seriesName="MSFT"
                         xScale={xScale}
                         xAccessor={xAccessor}
                         displayXAccessor={displayXAccessor}
                         xExtents={xExtents}
-                        style={{ Color: 'white' }}
+                        style={{ color: 'white' }}
                     >
                         <Chart id={1} yExtents={d => [d.high, d.low]}>
                             <XAxis axisAt="bottom" orient="bottom" />
--- src/main/fronted/src/components/membership.js
+++ src/main/fronted/src/components/membership.js
@@ -22,7 +22,7 @@ function Membership() {
                 setSuccess('회원가입이 성공적으로 완료되었습니다. 로그인 페이지로 이동합니다.');
                 setTimeout(() => {
                     window.location.href = '/';
-                }, 2000); // Redirect after 2 seconds
+                }, 2000);
             } else {
                 setError('회원가입 실패: ' + response.data.message);
             }
